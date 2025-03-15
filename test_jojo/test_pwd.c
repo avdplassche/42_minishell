ab19f3224042fd2d8aabaf8168969c2bc1d44009
Jojorandet <josephine@randet.com>
2025-03-15 17:11:03 +0100

    pwd, echo, cd started, structures modified for pipex, init mini updated for fildescriptor backup

---
+++ test_jojo/test_pwd.c
@@ -0,0 +1,30 @@
+
+#include "../includes/minishell.h"
+
+int	pwd(t_mini *mini)
+{
+	(void)mini;
+	
+	char	cwd[PATH_MAX];
+	char	*pwd_env;
+
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+	{
+		pwd_env = getenv("PWD");
+		printf("%s\n", pwd_env);
+	}
+	else
+	{
+		perror("get_cwd() failed\n");
+		return (-1);
+	}
+	return (0);
+}
+
+int main()
+{
+	t_mini mini;
+
+	pwd(&mini);
+	return (0);
+}
