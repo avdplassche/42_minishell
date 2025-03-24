23a18350c3136c0923c15b2e29c5a7adf065b76a
jrandet <jrandet@student.42.fr>
2025-03-24 20:55:52 +0100

    dev.sh improved,locals created in run_test

--- dev.sh
+++ dev.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
 
 make re
+
+MINISHELL="./minishell"
+TEST_DIR="./test_jojo/tests"
+RESULTS_DIR="./test_jojo/results"
+LOG_DIR="./test_jojo/logs"
+RESET="\033[0m"
+
+mkdir -p "$TEST_DIR" "$RESULTS_DIR" "$LOG_DIR"
+
+info(){
+	echo -e "\033[0;36m$1${RESET}"
+}
+
+success(){
+	echo -e "\033[0;32m$1${RESET}"
+}
+
+error(){
+	echo -e "\033[0;31m$1${RESET}"
+}
+
+warning(){
+	echo -e "\033[0;33m$1${RESET}"
+}
+
+run_test() {
+	local test_file="$TEST_DIR/simple.sh"
+	local test_name=$(basename "$test_file")
+	local output_dir=./log/$test_name
+	local output_standard_file="$output_dir/minishell.log"
+	
+	mkdir -p $output_dir
+
+	$LEAKS_CMD $MINISHELL $test_file > $output_standard_file
+}
+
+run_test
+
 LEAKS_CMD="valgrind"
 LEAKS_CMD+=" --leak-check=full"
 LEAKS_CMD+=" --track-fds=yes"
@@ -8,5 +46,3 @@ LEAKS_CMD+=" --show-leak-kinds=all"
 LEAKS_CMD+=" --track-origins=yes"
 LEAKS_CMD+=" --log-file=log/leaks.log"
 LEAKS_CMD+=" --trace-children=yes"
-
-$LEAKS_CMD ./minishell ./test_jojo/tests/simple.sh
--- test_mini.sh
+++ test_mini.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 
-GREEN="\033[0;32m"
-RED="\033[0;31m"
-BLUE="\033[0;34m"
-YELLOW="\033[0;33m"
-CYAN="\033[0;36m"
-RESET="\033[0m"
 
 MINISHELL="./minishell"
 TEST_DIR="./test_jojo/tests"
 RESULTS_DIR="./test_jojo/results"
 LOG_DIR="./test_jojo/logs"
+RESET="\033[0m"
 
 mkdir -p "$TEST_DIR" "$RESULTS_DIR" "$LOG_DIR"
 
 info(){
-	echo -e "${CYAN}$1${RESET}"
+	echo -e "\033[0;36m$1${RESET}"
 }
 
 success(){
-	echo -e "${GREEN}$1${RESET}"
+	echo -e "\033[0;32m$1${RESET}"
 }
 
 error(){
-	echo -e "${RED}$1${RESET}"
+	echo -e "\033[0;31m$1${RESET}"
 }
 
 warning(){
-	echo -e "${YELLOW}$1${RESET}"
+	echo -e "\033[0;33m$1${RESET}"
 }
 
 print_header() {
